e76cb2c71c990821cd29669c1d02e70fc0df93ab
Jacky <liyang@chinagate.com>
2015-11-03 16:58:45 -0800

    init

--- client/template/add-weixin-group.js
+++ client/template/add-weixin-group.js
@@ -1,37 +1,82 @@
 
-Template.addWeixinGroup.events({
-    'submit form': function(e){
-        e.preventDefault();
+Template.addWeixinGroup.helpers({
+    categoryList : function(){
+        return WeixinGroupCategory;
+    }
+});
 
-        var elem = $(e.target);
+var F = {
+    submit : function(callback){
+        var elem = $('.js_form');
 
         var data = {
             name : util.findRole(elem, 'name').val(),
-            category : util.findRole(elem, 'category').val(),
+            catId : util.findRole(elem, 'catId').val(),
             tag : util.findRole(elem, 'tag').val(),
             location : util.findRole(elem, 'location').val(),
             description : util.findRole(elem, 'description').val(),
 
-            owner : 'Jacky',
-            ownerId : 'jacky112233',
+            owner : '',
+            ownerId : util.findRole(elem, 'ownerId').val(),
             image : util.findRole(elem, 'image').val(),
-            qrCode : util.findRole(elem, 'qrCode').val()
+            qrCode : util.findRole(elem, 'qrCode').val(),
+            postCode : util.findRole(elem, 'postCode').val(),
+            memberCount : util.findRole(elem, 'memberCount').val()
         };
 
 
         console.log(data);
+
         WeixinGroup.insertData(data, function(err, rs){
             if(rs){
                 alert('添加成功');
 
-                Router.go('/');
+                callback(true)
             }
             else{
                 //console.log(err);
                 alert(err);
+                callback(false);
+            }
+        });
+    }
+};
+
+Template.addWeixinGroup.events({
+    //'submit form': function(e){
+    //    e.preventDefault();
+    //
+    //    var elem = $(e.target);
+    //
+    //
+    //},
+
+    'click .js_btn1' : function(e){
+        var o = $(e.target);
+        o.button('loading');
+
+        F.submit(function(f){
+            o.button('reset');
+
+            if(f){
+                Router.go('weixin-group-list');
             }
         });
     },
+    'click .js_btn2' : function(e){
+
+        var o = $(e.target);
+        o.button('loading');
+
+        F.submit(function(f){
+            o.button('reset');
+
+            if(f){
+                window.location.reload();
+            }
+        });
+    },
+
     'change [role="upload_image"]' : function(e){
         var files = e.target.files;
         if(files){
--- db/WeixinGroup.js
+++ db/WeixinGroup.js
@@ -14,37 +14,41 @@ WeixinGroup.attachSchema(new SimpleSchema({
     owner : {
         type : String,
         label : 'Group Owner',
-        optional : false
+        optional : true
     },
     ownerId : {
         type : String,
+        label : '群主微信号',
         optional : true
     },
     category : {
         type : String,
-        label : '分类',
-        optional : true
+        label : '群主题'
+    },
+    catId : {
+        type : String
     },
     description : {
         type : String,
-        label : '简介',
-        optional : true
+        label : '群简介',
+        optional : false
     },
     location : {
         type : String,
-        label : '区域',
-        optional : true
+        label : '群所在地区',
+        optional : false
     },
     image : {
-        type : String
+        type : String,
+        label : '群头像'
     },
     qrCode : {
         type : String,
-        label : '二维码',
-        optional : true
+        label : '群二维码'
     },
     tag : {
         type : String,
+        label : '群标签',
         optional : true
     },
     createTime : {
@@ -52,6 +56,16 @@ WeixinGroup.attachSchema(new SimpleSchema({
     },
     updateTime : {
         type : Date
+    },
+
+    postCode : {
+        type : String,
+        optional : true
+    },
+
+    memberCount : {
+        type : String,
+        optional : true
     }
 
 
@@ -73,16 +87,31 @@ WeixinGroup.addTestData = function(){
         location : 'Fremont',
         image : 'http://www.haiwai.com/images/hw_logo1.png',
         qrCode : '',
+        postCode : '99887',
+
         tag : '互联网，微信，创业群'
     };
 
-    WeixinGroup.insertData(data, function(){
-        console.log('add test data');
-    });
+    //TODO test
+
+    //WeixinGroup.insertData(data, function(){
+    //    console.log('add test data');
+    //});
 };
 
 _.extend(WeixinGroup, {
     insertData : function(data, callback){
+        var catId = data.catId,
+            category = _.find(WeixinGroupCategory, function(item){
+                return catId === item.id;
+            });
+        if(!category){
+            callback('请选择群主题', null);
+            return;
+        }
+        data.category = category.name;
+
+
         data.createTime = Date.now();
         data.updateTime = data.createTime;
         WeixinGroup.insert(data, function(err, uuid){
